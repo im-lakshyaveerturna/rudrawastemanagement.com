c92c378616fbf0576aa52631b06d96b0eb4e559e
Lakshyaveer Turna <th.is.lakshyaveerturna@gmail.com>
2025-12-02 00:16:12 +0530

    design more

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -2,25 +2,30 @@ import { useState } from 'react'
 import './Gallery.css'
 import CircularGallery from '../components/CircularGallery'
 import image1 from '../assets/gallery/image-1.jpg'
-import image2 from '../assets/gallery/image-2.jpg'
 import image3 from '../assets/gallery/image-3.jpg'
 import image4 from '../assets/gallery/image-4.jpg'
 import image5 from '../assets/gallery/image-5.jpg'
-import image6 from '../assets/gallery/image-6.jpg'
-import image7 from '../assets/gallery/image-7.jpg'
-import image8 from '../assets/gallery/image-8.jpg'
 import image9 from '../assets/gallery/iamge-9.jpeg'
 import image10 from '../assets/gallery/image-10.jpeg'
+// Services images to include in gallery
+import svcCollection from '../assets/services/doortodoor.png'
+import svcIncinerator from '../assets/services/incinerator.png'
+import svcAutoclave from '../assets/services/autoclave.png'
+import svcShredding from '../assets/services/shredding.jpg'
+import svcScrubber from '../assets/services/SCRUB.png'
 
 const images = [
+  // Services visuals first
+  { image: svcCollection, text: 'Door-to-door biomedical waste collection with authorized vehicles' },
+  { image: svcIncinerator, text: 'High-temperature incinerator for anatomical and pharmaceutical waste' },
+  { image: svcAutoclave, text: 'Autoclave steam sterilization for infectious biomedical waste' },
+  { image: svcShredding, text: 'Shredding facility for secure volume reduction and destruction' },
+  { image: svcScrubber, text: 'Scrubber system for stack emission control ensuring air safety' },
+  // Remaining gallery visuals
   { image: image1, text: 'Biomedical waste collection vehicle and facility' },
-  { image: image2, text: 'Waste treatment autoclave equipment' },
   { image: image3, text: 'Medical waste segregation and storage area' },
   { image: image4, text: 'Incineration facility for hazardous waste' },
   { image: image5, text: 'Waste collection team with safety gear' },
-  { image: image6, text: 'Shredding and volume reduction equipment' },
-  { image: image7, text: 'Emission control scrubber system' },
-  { image: image8, text: 'Waste treatment processing facility' },
   { image: image9, text: 'Color-coded biomedical waste containers' },
   { image: image10, text: 'Waste management facility operations' },
 ]
